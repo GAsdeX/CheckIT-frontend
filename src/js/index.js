f685ba7b9bd3976a9a69ab952f673e37eff00647
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@s4b.team>
2018-03-29 21:14:41 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var $ = window.$ = window.jQuery = require('jquery');
+var functions = require('./functions');
+
+/*
+ * Exported in 'functions.js' functions can be used here:
+ * functions.exportName()
+ */
+
+$(function () {
+
+	// Code goes here
+
+});
