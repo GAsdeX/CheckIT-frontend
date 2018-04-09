fa53c0bde37cbf0ed4ddcb96eef0a75129f54bfd
akjeres <akjeres@gmail.com>
2018-04-09 22:10:53 +0300

    userpage profile photo scripts added

--- src/js/functions.js
+++ src/js/functions.js
@@ -10,3 +10,12 @@ module.exports = {
 };
 
 // Code goes here
+
+document.querySelector(".remove-photo").onclick = function() {
+	console.log('Profile\'s photo has to be removed');
+	document.querySelector(".user-photo").classList.add("default");
+}
+document.querySelector(".upload-photo").onclick = function() {
+	console.log('Profile\'s photo has to be uploaded to the server');
+	document.querySelector(".user-photo").classList.remove("default");
+}
