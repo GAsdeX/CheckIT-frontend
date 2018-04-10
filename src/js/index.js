10a4a92bb9726c2fea9de731129f245394a51bff
akjeres <akjeres@gmail.com>
2018-04-10 23:07:41 +0300

    Replaced handlers file locations

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,13 @@ var functions = require('./functions');
 $(function () {
 
 	// Code goes here
+    document.querySelector(".remove-photo").onclick = function() {
+        console.log('Profile\'s photo has to be removed');
+        document.querySelector(".user-photo").classList.add("default");
+    };
+    document.querySelector(".upload-photo").onclick = function() {
+        console.log('Profile\'s photo has to be uploaded to the server');
+        document.querySelector(".user-photo").classList.remove("default");
+    };
 
 });
