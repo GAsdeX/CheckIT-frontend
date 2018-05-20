317aaabe1df163178d6abea17e60bdfcd7788388
akjeres <akjeres@gmail.com>
2018-05-20 16:36:04 +0300

    About page completed

--- src/vue/pages/about.vue
+++ src/vue/pages/about.vue
@@ -6,17 +6,94 @@
                 .row
                     .col-md-6
                         .about-box
-                            p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto, aut eos exercitationem facere facilis fugiat inventore iste laborum libero nesciunt nobis odit optio, quibusdam recusandae temporibus totam voluptas voluptate?
-                            p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto, aut eos exercitationem facere facilis fugiat inventore iste laborum libero nesciunt nobis odit optio, quibusdam recusandae temporibus totam voluptas voluptate?
+                            p Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                | Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                | when an unknown printer took a galley of type and scrambled it to make a
+                                | type specimen book. It has survived not only five centuries.
+                                | Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                | when an unknown printer took a galley of type and scrambled it to make a type
+                                | specimen book. It has survived not only five centuries.
+                            p Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                | Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                | when an unknown printer took a galley of type and scrambled it to make a
+                                | type specimen book. It has survived not only five centuries.
+                                | Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                | when an unknown printer took a galley of type and scrambled it to make a type
+                                | specimen book. It has survived not only five centuries.
 
-                    .col-md-6
             .container
                 h2.section-title Our team
                 .row
-                    .col-md-6
-
-                    .col-md-6
-
+                    .col-md-3
+                        .member-card.chaikovskyi
+                            .member-photo
+                            .member-info
+                                h4.member-name Grigoriy Chaikovskyi
+                                p.member-rank Front End Developer
+                                a(href="mailto:hryhorii.chaikovskyi@gmail.com").member-email hryhorii.chaikovskyi@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.dovzhenko
+                            .member-photo
+                            .member-info
+                                h4.member-name Gleb Dovzhenko
+                                p.member-rank Back End Developer
+                                a(href="mailto:dovzhenko.gleb@gmail.com").member-email dovzhenko.gleb@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.kravchenko
+                            .member-photo
+                            .member-info
+                                h4.member-name Irina Kravchenko
+                                p.member-rank QA Engineer
+                                a(href="mailto:doroshenko123@gmail.com").member-email doroshenko123@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.rybko
+                            .member-photo
+                            .member-info
+                                h4.member-name Irina Rybcko
+                                p.member-rank UI/UX Designer
+                                a(href="mailto:irarybcko@gmail.com").member-email irarybcko@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.shevchenko
+                            .member-photo
+                            .member-info
+                                h4.member-name Anna Shevchenko
+                                p.member-rank PM, Front End Developer
+                                a(href="mailto:annashevchenko.od@gmail.com").member-email annashevchenko.od@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.smorodskiy
+                            .member-photo
+                            .member-info
+                                h4.member-name Vlad Smorodskiy
+                                p.member-rank Back End Developer
+                                a(href="mailto:smorodsky.odesa@gmail.com").member-email smorodsky.odesa@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
+                    .col-md-3
+                        .member-card.tkach
+                            .member-photo
+                            .member-info
+                                h4.member-name Serhii Tkach
+                                p.member-rank Front End Developer
+                                a(href="akjeres@gmail.com").member-email akjeres@gmail.com
+                                .member-social
+                                    a(href="https://www.facebook.com/").member-social-link.facebook
+                                    a(href="https://www.linkedin.com/").member-social-link.linkedin
 </template>
 
 <script>
