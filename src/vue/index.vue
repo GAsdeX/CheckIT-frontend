3efdd59b9a843db2a3d3a6c9ad31ddd9fb69d7da
akjeres <akjeres@gmail.com>
2018-05-22 01:35:14 +0300

    Author ideas page markup completed

--- src/vue/index.vue
+++ src/vue/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="global-site-wrapper">
+	<div class="global-site-wrapper" v-on:click="removeActive">
         <modal-window></modal-window>
         <div class="site-top-content">
             <site-header></site-header>
@@ -14,6 +14,15 @@
 
 <script>
     module.exports = {
+        methods:    {
+            removeActive: function() {
+                if (!(event.path[0].classList.contains('delete-idea') || event.path[0].classList.contains('more-button'))) {
+                    for (var i = 0; i < document.querySelectorAll('.editor-list').length; i++) {
+                        document.querySelectorAll('.editor-list')[i].classList.remove('active');
+                    }
+                }
+            }
+        },
         components: {
             'site-header': require('../vue/components/site-header.vue'),
             'site-footer': require('../vue/components/site-footer.vue'),
--- src/vue/pages/author-ideas.vue
+++ src/vue/pages/author-ideas.vue
@@ -11,31 +11,113 @@
                     li.authors-nav-links-item(:class="{active: tab === 'liked'}")
                         a(@click="setTab('liked')") Liked
             .container
-                h2.section-title You have drafts
+                h2.section-description You have&nbsp;
+                    |
+                    span.section-amount.drafts(v-if="tab == 'drafts'") 5
+                    span.section-amount.published(v-if="tab == 'published'") 3
+                    span.section-amount.liked(v-if="tab == 'liked'") 4
+                    |
+                    span.section-description-after.drafts(v-if="tab == 'drafts'") &nbsp;drafts
+                    span.section-description-after.published(v-if="tab == 'published'") &nbsp;published ideas
+                    span.section-description-after.liked(v-if="tab == 'liked'") &nbsp;liked ideas
 
                 .row
                     .col-md-3(v-if="tab == 'published'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.moderated.approved
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'published'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.moderated.rejected
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'published'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.moderated.approved
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'liked'")
                         idea-item()
                     .col-md-3(v-if="tab == 'liked'")
                         idea-item()
                     .col-md-3(v-if="tab == 'liked'")
                         idea-item()
-                    .col-md-3(v-if="tab == 'drafts'")
+                    .col-md-3(v-if="tab == 'liked'")
                         idea-item()
                     .col-md-3(v-if="tab == 'drafts'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.draft
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'drafts'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.draft
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'drafts'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.draft
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
                     .col-md-3(v-if="tab == 'drafts'")
                         idea-item()
+                        ul.editor-section
+                            li.editor-item.draft
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
+                    .col-md-3(v-if="tab == 'drafts'")
+                        idea-item()
+                        ul.editor-section
+                            li.editor-item.draft
+                            li.editor-item.more
+                                button(@click="editorController('toggle')").editor-item.more-button
+                                ul.editor-list
+                                    li.editor-list-item.edit
+                                        a.edit-idea(href="#") Edit Idea
+                                    li.editor-list-item.delete
+                                        button(@click="callModal('delete')").editor-item.more-button.delete-idea Delete idea
 
 
 </template>
@@ -50,6 +132,23 @@
         methods: {
             setTab: function (tab) {
                 this.tab = tab;
+            },
+            editorController: function(action) {
+                var that = event.path[1].lastChild;
+                var flag = that.classList.contains('active');
+                for (var i = 0; i < document.querySelectorAll('.editor-list').length; i++) {
+                    document.querySelectorAll('.editor-list')[i].classList.remove('active');
+                }
+                if (action === 'toggle') {
+                    if (flag) {
+                        that.classList.remove('active');
+                    } else {
+                        that.classList.add('active');
+                    };
+                };
+            },
+            callModal: function(component) {
+                this.$eventBus.$emit("callModal", {action: component})
             }
         },
         components:{
